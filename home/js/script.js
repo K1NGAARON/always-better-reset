568c9373bbf73748006fbbe15c22d4378087fe6b
K1NGAARON <gerardaaron1997@gmail.com>
2023-05-23 10:46:47 +0200

    implemented script

--- home/js/script.js
+++ home/js/script.js
@@ -1,87 +1,97 @@
 const servicesContent = [
     {
-        title: "",
-        id: "",
-        img: "",
-        body: "",
+        title: "Facial Work",
+        id: "facial-work",
+        img: "/home/img/services/image1.jpg",
+        body: "facial work body",
     },
     {
-        title: "",
-        id: "",
-        img: "",
-        body: "",
+        title: "Accupuncture",
+        id: "accupuncture",
+        img: "/home/img/services/image2.jpg",
+        body: "accupuncture body",
     },
     {
-        title: "",
-        id: "",
-        img: "",
-        body: "",
+        title: "Cupping",
+        id: "cupping",
+        img: "/home/img/services/image3.jpg",
+        body: "cupping body",
     },
     {
-        title: "",
-        id: "",
-        img: "",
-        body: "",
+        title: "Movement Analysis",
+        id: "movement-analysis",
+        img: "/home/img/services/image4.jpg",
+        body: "movement analysis body",
     },
     {
-        title: "",
-        id: "",
-        img: "",
-        body: "",
+        title: "Breathwork Analysis",
+        id: "breathwork-analysis",
+        img: "/home/img/services/image5.jpg",
+        body: "breathwork analysis body",
     },
     {
-        title: "",
-        id: "",
-        img: "",
-        body: "",
+        title: "Bloodwork Analysis",
+        id: "bloodwork-analysis",
+        img: "/home/img/services/image6.jpg",
+        body: "bloodwork analysis body",
     },
 ];
 
 const experiencesContent = [
     {
-        title: "",
-        id: "",
-        img: "",
-        body: "",
+        title: "Breathwork classes",
+        id: "breathwork-classes",
+        img: "/home/img/services/image3.jpg",
+        body: "breathwork classes body",
     },
     {
-        title: "",
-        id: "",
-        img: "",
-        body: "",
+        title: "Bodywork classes",
+        id: "bodywork-classes",
+        img: "/home/img/services/image2.jpg",
+        body: "bodywork body",
     },
     {
-        title: "",
-        id: "",
-        img: "",
-        body: "",
+        title: "Breathwork 1-on-1",
+        id: "breathwork",
+        img: "/home/img/services/image1.jpg",
+        body: "breathwork body",
     },
     {
-        title: "",
-        id: "",
-        img: "",
-        body: "",
+        title: "Re set trips",
+        id: "reset-trips",
+        img: "/home/img/services/image4.jpg",
+        body: "re set body",
     },
 ];
 
-
-function filterContent(e) {
-    // Toggle Active Class on Filter
-    $('.filter-btn').removeClass('active');
+function filterServices(e) {
+    const filterWrapper = $(this).closest('.filters');
+    filterWrapper.find('.filter-btn').removeClass('active');
     $(this).addClass('active');
 
-
-
-    // 
-    $('.job-openings .card').fadeOut();
-
-    // Selected Tag
     let activeFilter = $(this).attr('id');
 
-    if (activeFilter === 'all') {
-        $('.job-openings .card').fadeIn();
-    } else {
-        $('.' + activeFilter).fadeIn();
-    }
+    const selectedService = servicesContent.find(service => service.id === activeFilter);
+
+    $('#service-img').attr('src', selectedService.img);
+    $('#service-body').text(selectedService.body);
 };
 
+function filterExperiences(e) {
+    const filterWrapper = $(this).closest('.filters');
+    filterWrapper.find('.filter-btn').removeClass('active');
+    $(this).addClass('active');
+
+    let activeFilter = $(this).attr('id');
+
+    const selectedExperience = experiencesContent.find(experience => experience.id === activeFilter);
+   
+    $('#experiences-img').attr('src', selectedExperience.img);
+    $('#experiences-body').text(selectedExperience.body);
+}
+
+
+$(document).ready(function() {
+    $('.filters-services .filter-btn').click(filterServices);
+    $('.filters-experiences .filter-btn').click(filterExperiences);
+});
